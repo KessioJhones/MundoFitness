a454251d6ba2820f59632aba3de53c8a1762b6c3
Thiago <developerkeey@gmail.com>
2023-09-22 08:29:31 -0400

    theme dark and light

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 AOS.init();
+
+const bodyy = document.querySelector(".light-mode");
+const light = document.querySelector(".light-theme");
+
+light.addEventListener('click', ()=>{
+    bodyy.classList.toggle ('light');
+});
